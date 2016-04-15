1d1d1f8ca5da464ff5a6dd7b5fea76a8907c09b0
andreayng <andrea.y.ng@gmail.com>
2016-04-14 23:24:56 -0400

    Added throws declaration to pass shouldThrowIllegalArgumentExceptionWhenNumberIsNegative

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,7 +2,10 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i == 0 || i == 1) {
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
+        else if (i == 0 || i == 1) {
             return 1;
         }
         else {
